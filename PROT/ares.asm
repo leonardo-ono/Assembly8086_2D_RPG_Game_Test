01e2263a1ed1db2f2712b8b91ef1c996f8248b06
unknown <ono.leo@gmail.com>
2021-05-31 14:16:56 -0300

    very old assembly 8086 2d rpg game test / prototype i found recently.

---
+++ PROT/ares.asm
@@ -0,0 +1,22 @@
+;----------------------------------------------------------------------------;
+;          Dados: Contàm todas os FRAMES do personagem "ARES"                ;
+;----------------------------------------------------------------------------;
+;           Obs.: estes dados dever∆o ser inclusas no segmento               ;
+;                 'rpg_personagens'.                                         ;
+;----------------------------------------------------------------------------;
+                INCLUDE ares\prsp10.asm              ; - Pers Princ: Frame CIMA 1
+                INCLUDE ares\prsp11.asm              ;               Frame CIMA 2
+                INCLUDE ares\prsp12.asm              ;               Frame CIMA 3
+                INCLUDE ares\prsp11.asm              ;               Frame CIMA 2
+                INCLUDE ares\prsp07.asm              ; - Pers Princ: Frame BAIX 1
+                INCLUDE ares\prsp08.asm              ;               Frame BAIX 2
+                INCLUDE ares\prsp09.asm              ;               Frame BAIX 3
+                INCLUDE ares\prsp08.asm              ;               Frame BAIX 2
+                INCLUDE ares\prsp04.asm              ; - Pers Princ: Frame DIRE 1
+                INCLUDE ares\prsp05.asm              ;               Frame DIRE 2
+                INCLUDE ares\prsp06.asm              ;               Frame DIRE 3
+                INCLUDE ares\prsp05.asm              ;               Frame DIRE 2
+                INCLUDE ares\prsp01.asm              ; - Pers Princ: Frame ESQU 1
+                INCLUDE ares\prsp02.asm              ;               Frame ESQU 2
+                INCLUDE ares\prsp03.asm              ;               Frame ESQU 3
+                INCLUDE ares\prsp02.asm              ;               Frame ESQU 2
